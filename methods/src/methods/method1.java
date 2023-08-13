6944618e5720be1bb6a2eeffdabfc30524c34cf2
Narayana1436 <141153566+Narayana1436@users.noreply.github.com>
2023-08-13 16:49:39 +0530

    Update method1.java

--- methods/src/methods/method1.java
+++ methods/src/methods/method1.java
@@ -4,11 +4,11 @@ public class method1 {
 
 	public static void main(String[] args) {
 		System.out.println(8);
-		lasya();
+		Kodnest();
 		System.out.println(10);
 
 	}
-public static void lasya() {
+public static void Kodnest() {
 	System.out.println(9);
 }
 }
