8bdf48364e4770425757e61bf5e4de9afe6a3130
Narayana1436 <141153566+Narayana1436@users.noreply.github.com>
2023-08-13 16:53:05 +0530

    my programs

--- methods/src/methods/method1.java
+++ methods/src/methods/method1.java
@@ -4,11 +4,11 @@ public class method1 {
 
 	public static void main(String[] args) {
 		System.out.println(8);
-		Kodnest();
+		KodNest();
 		System.out.println(10);
 
 	}
-public static void Kodnest() {
+public static void KodNest() {
 	System.out.println(9);
 }
 }
